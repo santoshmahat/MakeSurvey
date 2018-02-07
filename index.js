5aa1de471fdfaf238a9351aec01fa73bf1a76738
DELL <sarita@gmail.com>
2018-02-07 11:44:01 +0545

    update in auth flow

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const keys = require('./config/keys');
+mongoose.connect(keys.mongoURI);
+require('./models/Users');
+require('./services/passport');
+
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send({'hello':'santosh'});
+app.use(
+  cookieSession({
+      maxAge:30*24*60*60*1000,
+      keys:[keys.cookieKey]
 })
-
-
+);
+app.use(passport.initialize());
+app.use(passport.session());
+require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
